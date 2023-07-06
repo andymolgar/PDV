97e3f848ffd46986514fbf744a945225630d5323
andymolgar <94236985+andymolgar@users.noreply.github.com>
2023-07-06 10:17:16 -0300

    Aula 08- Criando a Tela de Fechamento do Caixa

---
+++ 08- Criando a Tela de Fechamento do Caixa/src/view/componente/ACPDV.View.Componente.Transparencia.pas
@@ -0,0 +1,24 @@
+unit ACPDV.View.Componente.Transparencia;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TpageTransparencia = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  pageTransparencia: TpageTransparencia;
+
+implementation
+
+{$R *.dfm}
+
+end.
